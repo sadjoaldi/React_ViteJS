937a8f46b8e466ce34412d1bcbf29974d287f329
Sadjoaldi <alsannealdi@outlook.com>
2023-10-10 11:05:27 +0200

    first component

---
+++ src/Components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+const PokemonCard = () => {
+  return (
+    <div>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt=""
+        />
+      </figure>
+      <figcaption>Bulbasaur</figcaption>
+    </div>
+  );
+};
+
+export default PokemonCard;
